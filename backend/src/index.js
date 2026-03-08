4107a3783f8f412c10f823ed418783a3bd692b7e
joshiyj <yashjoshi7972@gmail.com>
2026-03-08 23:47:40 +0530

    Add message model and message routes for chat functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
      
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
