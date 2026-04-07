b856031a4e866b58b91b73a7cb74cb3742bbcd53
joshiyj <yashjoshi7972@gmail.com>
2026-04-08 00:11:50 +0530

    fix: route error

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,10 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/*", (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     });
 }
